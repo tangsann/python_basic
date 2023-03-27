ff504b73983977917cbb93e88e37e728d8e179df
tangsan <lishaohuii@outlook.com>
2023-03-27 09:23:44 +0800

    add five

--- five/list_copy.py
+++ five/list_copy.py
@@ -3,6 +3,8 @@
 l1 = [4,5,7,1,3,9,0]
 l2 = l1.copy()
 l2.sort()
+#copy和sort，可以使用下面一条代码代替
+#l2 = sorted(l1)
 for i in range(len(l1)):
     print(l1[i],l2[i])
 
--- five/正则表达式练习.py
+++ five/正则表达式练习.py
@@ -3,5 +3,6 @@
 import os
 import re
 route_n_result = os.popen('route -n').read()
+#ipv4_gw = re.findall(r'0.0.0.0\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+0.0.0.0\s+UG', route_n_result)[0]
 ipv4_gw = re.match('^Kernel[\s\S]+0.0.0.0\s+(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})\s+0.0.0.0\s+UG[\s\S]+$',route_n_result).groups()
 print('网关为:' + ipv4_gw[0])
