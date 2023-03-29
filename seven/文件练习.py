f1134585a8e577b8cd8be26b55f7734756a83b0e
tangsan <lishaohuii@outlook.com>
2023-03-29 21:58:04 +0800

    add seven

---
+++ seven/文件练习.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# coding=utf-8
+import os
+os.mkdir('test')
+os.chdir('test')
+qytang1 = open('qytang1','w')
+qytang1.write('test file\n')
+qytang1.write('this is qytang\n')
+qytang1.close()
+qytang2 = open('qytang2','w')
+qytang2.write('test file\n')
+qytang2.write('qytang python\n')
+qytang2.close()
+qytang3 = open('qytang3','w')
+qytang3.write('test file\n')
+qytang3.write('this is python\n')
+qytang3.close()
+os.mkdir('qytang4')
+os.mkdir('qytang5')
+for root,dirs,files in os.walk(os.getcwd(),topdown=False):
+    for name in files:
+        if 'qytang' in open(os.path.join(root,name)).read():
+            print(name)
+os.chdir('..')
+for root,dirs,files in os.walk('test',topdown=False):
+    for name in files:
+        os.remove(os.path.join(root,name))
+    for name in dirs:
+        os.rmdir(os.path.join(root,name))
+
+os.removedirs('test')
+
+
+
+
+
+
+
+
