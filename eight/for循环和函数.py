bcc007509d08badcca888f46d53af812a554b211
tangsan <lishaohuii@outlook.com>
2023-03-30 21:24:17 +0800

    add eight

--- eight/for循环和函数.py
+++ eight/for循环和函数.py
@@ -8,13 +8,12 @@ for x in list1:
         print(f'{x} in list1 and list2')
     else:
         print(f'{x} only in list1')
-#方案er
+#方案二
 def find_same(l1,l2):
     for x in list1:
         if x in list2:
             print(f'{x} in list1 and list2')
         else:
             print(f'{x} only in list1')
-
 if __name__ =='__main__':
     find_same(list1,list1)
