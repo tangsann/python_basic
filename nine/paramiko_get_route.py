a55e36feec3ac596f14d442ac514cf68e627385a
tangsan <lishaohuii@outlook.com>
2023-04-01 18:21:08 +0800

    add

--- nine/paramiko_get_route.py
+++ nine/paramiko_get_route.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # coding=utf-8
 import re
-from nine import paramiko练习
+import paramiko练习_ssh
 def ssh_get_route(ip,username,password,cli='route -n'):
-    route_result = paramiko练习.get_ssh(ip,username,password,cmd=cli)
+    route_result = paramiko练习_ssh.get_ssh(ip,username,password,cmd=cli)
     ipv4_gw = re.match(r'^Kernel[\s\S]+0.0.0.0\s+(\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3})\s+0.0.0.0\s+UG[\s\S]+$', route_result).groups()[0]
     return ipv4_gw
 if __name__ == '__main__':
