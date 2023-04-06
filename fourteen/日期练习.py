b7fff39b8f9b3f289fca626628ee4a437b4b5753
tangsan <lishaohuii@outlook.com>
2023-04-06 11:06:05 +0800

    add

---
+++ fourteen/日期练习.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding=utf-8 -*-
+from datetime import datetime,timedelta
+
+five_days_ago = datetime.now() - timedelta(days=5)
+five_days_ago_str_1 = five_days_ago.strftime('%Y-%m-%d_%H-%M-%S')
+five_days_ago_str_2 = str(five_days_ago)
+filename = 'save_fivedaysago_time_'+five_days_ago_str_1
+wf = open(filename+'.txt','w')
+wf.write(five_days_ago_str_2)
+wf.close()
